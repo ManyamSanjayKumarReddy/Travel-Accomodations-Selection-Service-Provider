28e852afd844836cdfec65bce2e945e906209e74
ManyamSanjayKumarReddy <entrepreneur.sanju@gmail.com>
2023-09-29 15:16:58 +0530

    Final Fixings and Bug Removal

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('delete_booking/<int:order_id>/', views.delete_booking, name='delete_booking'),
     path('update_appointment_date/<int:order_id>/', views.update_appointment_date, name='update_appointment_date'),
     path('dashboard/', views.dashboard, name='dashboard'),
-
-
+    
+    
 ]
